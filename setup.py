a3f6fd5cb0f2636023d4887144cde108c7d9033a
meanstrong <pmq2008@gmail.com>
2021-03-18 11:29:27 +0800

    update version

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@
 
 from setuptools import setup, find_packages
 
+import deploy4s
+
+
 with open("README.md", encoding="utf-8") as f:
     readme = f.read()
 
-with open("LICENSE", encoding="utf-8") as f:
-    license = f.read()
-
 setup(
     name="deploy4s",
-    version="1.0.3",
+    version=deploy4s.__version__,
     packages=find_packages(exclude=["test*"]),
     install_requires=["PyYAML"],
     zip_safe=False,
 
     url="https://github.com/meanstrong/deploy4s",
-    # license=license,
     description="A deploy tools for CodeDeploy",
     long_description=readme,
     long_description_content_type="text/markdown",
@@ -26,7 +25,7 @@ setup(
     maintainer="pengmingqiang",
     maintainer_email="rockypengchina@outlook.com",
     platforms=['any'],
-
+    license="GPLv3",
     entry_points={
         'console_scripts': [
             'deploycli = deploy4s.cli:main',
